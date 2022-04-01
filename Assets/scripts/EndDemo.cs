fd9d3ae5aebeb5cdbe92093115c8dc4c54f92987
Mees Hollebeek <Mees.hollebeek@gmail.com>
2022-04-01 15:15:11 +0200

    end of demo

---
+++ Assets/scripts/EndDemo.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndDemo : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject look;
+    [SerializeField] private Image  _noteImage;
+    void start()
+    {
+        player.GetComponent<PlayerMovement>().enabled = true;
+        look.GetComponent<MouseLook>().enabled = true;
+    }
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        player.GetComponent<PlayerMovement>().enabled = false;
+        look.GetComponent<MouseLook>().enabled = false;
+        if (other.CompareTag("Player"))
+        {
+            _noteImage.enabled = true;
+           
+        }
+
+    }
+
+    // Update is called once per frame
+    void OnTriggerExit(Collider other)
+    {
+        player.GetComponent<PlayerMovement>().enabled = false;
+        look.GetComponent<MouseLook>().enabled = false;
+       
+        if (other.CompareTag("Player"))
+        {
+            _noteImage.enabled = false;
+
+        }
+
+    }
+}
